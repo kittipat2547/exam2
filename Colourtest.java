f548a4028d053a81e623c1b05ce7c832a73e10cc
Nattaporn Kanjanarujeerat <154008042+Nattaporn1@users.noreply.github.com>
2024-02-03 21:27:49 +0700

    Create Colourtest.java

---
+++ Colourtest.java
@@ -0,0 +1,15 @@
+public class Colourtest {
+    public static void main(String[] args) {
+        Colour ojb1 = new Colour();
+        ojb1.setRed(22);
+        System.out.println(ojb1.getRed());
+
+        Colour obj2 = new Colour();
+        obj2.setBlue(11);
+        System.out.println(obj2.getBlue());
+
+        Colour obj3 = new Colour();
+        obj3.setGreen(33);
+        System.out.println(obj3.getGreen());
+    }
+}
